3f2cf510d38be6f9456ce521b5dd8c57bbec6401
AlgidPenguin <daniel_r129@hotmail.com>
2021-02-14 15:59:30 -0800

    Issue#26 registration (#29)
    * Created user account model
    
    * Added user account to database using code first migration
    
    * Created RegisterViewModel
    
    * Added a register page and more validation
    
    * Add registered user to the database

---
+++ eCommerceSite/Models/UserAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        [Compare(nameof(Email))]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 120")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string confirmPassword { get; set; }
+
+        [DataType(DataType.Date)] // Time ignored
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
