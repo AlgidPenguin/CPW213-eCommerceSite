00144bd691dea8739eed5b19f1315980b93fb402
Daniel Ramirez <daniel_r129@hotmail.com>
2021-02-14 15:34:20 -0800

    Added a register page and more validation

--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -23,6 +23,8 @@ namespace eCommerceSite.Models
     public class RegisterViewModel
     {
         [Required]
+        [EmailAddress]
+        [StringLength(200)]
         public string Email { get; set; }
 
         [Compare(nameof(Email))]
@@ -30,10 +32,12 @@ namespace eCommerceSite.Models
 
         [Required]
         [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 120")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
         public string confirmPassword { get; set; }
 
         [DataType(DataType.Date)] // Time ignored
