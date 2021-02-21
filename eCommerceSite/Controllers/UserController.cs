e6aa47796b827fdacf030bf45955fc137b1f874c
AlgidPenguin <daniel_r129@hotmail.com>
2021-02-21 14:40:10 -0800

    Issue#27 user login (#32)
    * Created the login page
    
    * Added login error message for invalid credentials
    
    * Enabled sessions
    
    * Added note about where to use sessions in Startup
    
    * Added session management for login page
    
    * Registered HttpContext service to access session in layout page
    
    * Added greeting to navigation if user is logged in
    
    * Added username greeting to navigation when logged in

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,6 +46,54 @@ namespace eCommerceSite.Controllers
                 return RedirectToAction("Index", "Home");
             }
             return View(reg);
+        }
+
+        public IActionResult Login()
+        {
+            // Check if user already logged in
+            if(HttpContext.Session.GetInt32("UserId").HasValue)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            UserAccount account =
+                await (from u in _context.UserAccounts
+                       where (u.Username == model.Username
+                           || u.Email == model.Username)
+                           && u.Password == model.Password
+                       select u).SingleOrDefaultAsync();
+
+            //UserAccount account1 =
+            //    await _context.UserAccounts
+            //        .Where(userAcc => (userAcc.Username == model.Username ||
+            //                userAcc.Email == model.Username) &&
+            //                userAcc.Password == model.Password).SingleOrDefaultAsync();
+
+            if(account == null)
+            {
+                // credentials didnt match
+
+                // custom error message
+                ModelState.AddModelError(string.Empty, "Credentials were not found.");
+
+                return View(model);
+            }
+
+            // Log in user to website
+            HttpContext.Session.SetInt32("UserId", account.UserId);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
