be968b2ffa4897aa8ce9f5b8b33f3467e637fd99
Daniel Ramirez <daniel_r129@hotmail.com>
2021-02-27 16:39:40 -0800

    Added shopping cart link to nav bar and Add to Cart button for each product on catalog page

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Add(int id) // Id of product to add to the cart
+        {
+            // Get product from database
+            // Add product to the cart cookie
+
+            // Redirect back to previous page
+            return View();
+        }
+
+        public IActionResult Summary()
+        {
+            // Display all products currently in the shopping cart cookie
+            return View();
+        }
+    }
+}
